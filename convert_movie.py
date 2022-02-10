04c3d75713bef9c7582f02312b6b35371642d607
Sasha Volokh <volokh@usc.edu>
2022-02-09 17:39:27 -0800

    initial commit

---
+++ convert_movie.py
@@ -0,0 +1,360 @@
+'''
+Common functions and utilities
+
+2022 Sasha Volokh
+'''
+
+import maya.cmds as cmds
+import maya.utils
+import os.path
+import time
+import threading
+import subprocess
+import json
+import re
+
+def popen(cmd, stdout, stderr):
+    if cmds.about(windows=True):
+        CREATE_NO_WINDOW = 0x08000000
+        return subprocess.Popen(cmd, stdout=stdout, stderr=stderr, creationflags=CREATE_NO_WINDOW)
+    else:
+        return subprocess.Popen(cmd, stdout=stdout, stderr=stderr)
+
+def isValidCommand(cmd):
+        try:
+            with open(os.devnull, 'w') as fnull:
+                popen(cmd, stdout=fnull, stderr=fnull)
+            return True
+        except OSError:
+            return False
+
+def getConfigPath():
+    scriptsDir = os.path.dirname(os.path.realpath(__file__))
+    name = os.path.splitext(os.path.basename(__file__))[0] + '.config.json'
+    return os.path.join(scriptsDir, name)
+
+def getOutputLogPath():
+    scriptsDir = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(scriptsDir, 'ffmpeg_output.txt')
+
+def readSettings():
+    cfgPath = getConfigPath()
+    if os.path.exists(cfgPath):
+        with open(cfgPath, 'r') as f:
+            return json.load(f)
+    else:
+        return dict()
+
+def writeSettings(settings):
+    cfgPath = getConfigPath()
+    with open(cfgPath, 'w') as f:
+        json.dump(settings, f)
+
+def getDefaultOperatingSystem():
+    settings = readSettings()
+    if 'operatingSystem' in settings:
+        return settings['operatingSystem']
+    elif cmds.about(windows=True):
+        return 'PC'
+    else:
+        return 'MAC'
+
+def getDefaultFFMpeg(operatingSystem):
+    settings = readSettings()
+    scriptsDir = os.path.dirname(os.path.realpath(__file__))
+    if operatingSystem == 'PC':
+        defaultCmd = os.path.join(scriptsDir, 'ffmpeg', 'bin', 'ffmpeg.exe')
+    else:
+        defaultCmd = os.path.join(scriptsDir, 'ffmpeg')
+    if 'ffmpegCommand' in settings and isValidCommand(settings['ffmpegCommand']):
+        return settings['ffmpegCommand']
+    elif isValidCommand('ffmpeg'):
+        return 'ffmpeg'
+    elif isValidCommand(defaultCmd):
+        return defaultCmd
+    else:
+        if 'ffmpegCommand' in settings:
+            return settings['ffmpegCommand']
+        else:
+            return defaultCmd
+
+def getMovieResolution(ffmpegCommand, inputMoviePath):
+    cmd = [ffmpegCommand, '-nostdin', '-y', '-i', inputMoviePath, '-f', 'null', '-']
+    outputFilePath = getOutputLogPath()
+    with open(outputFilePath, 'w') as outputFd:
+        p = popen(cmd, outputFd, outputFd)
+        if p.wait() != 0:
+            raise Exception('ffmpeg command failed for determining resolution')
+    with open(outputFilePath, 'r') as fd:
+        while True:
+            line = fd.readline()
+            if len(line) == 0:
+                raise Exception('could not determine movie resolution')
+            if line.find('Video:') > 0:
+                m = re.search('(\\d+)x(\\d+)', line.replace(' 0x', ''))
+                if not m:
+                    raise Exception('failed to determine movie resolution from ffmpeg output')
+                return int(m.group(1)), int(m.group(2))
+
+def run():
+    if cmds.window('ConvertMovie', exists=True):
+        cmds.deleteUI('ConvertMovie', window=True)
+
+    w = cmds.window('ConvertMovie', width=355, height=400, title='Convert Movie', menuBar=True)
+    l = cmds.columnLayout(parent=w, columnAttach=('both', 5), rowSpacing=5, columnWidth=350, width=350)
+
+    def openHelpMenu(*args):
+        cmds.showHelp('https://docs.google.com/document/d/1XVG1hAOgN7OIce_GG3SmsO9xnhpXGswg4ClSeiGN6ao/edit?usp=sharing', absolute=True)
+
+    m = cmds.menu(label='Help', helpMenu=True, parent=w)
+    cmds.menuItem(label='Tutorial', parent=m, command=openHelpMenu)
+
+    def resetMovieResolution():
+        inputMoviePath = cmds.textField(inputTextField, q=True, text=True)
+        ffmpegCommand = cmds.textField(ffmpegTextField, q=True, text=True)
+        if not isValidCommand(ffmpegCommand):
+            return
+        if len(inputMoviePath) == 0:
+            return
+        try:
+            w, h = getMovieResolution(ffmpegCommand, inputMoviePath)
+            cmds.text(sourceSizeText, edit=True, label='Source Size: {} px x {} px'.format(w, h))
+            cmds.textField(widthTextField, edit=True, text=str(w))
+            cmds.textField(heightTextField, edit=True, text=str(h))
+        except Exception as e:
+            print('failed to get movie resolution: {}'.format(e))
+            cmds.text(sourceSizeText, edit=True, label='Source Size: Unknown')
+            cmds.textField(widthTextField, edit=True, text='')
+            cmds.textField(heightTextField, edit=True, text='')
+
+    def checkFFMpeg():
+        ffmpegCmd = cmds.textField(ffmpegTextField, q=True, text=True)
+        if isValidCommand(ffmpegCmd):
+            cmds.textField(ffmpegTextField, edit=True, backgroundColor=(0.0, 0.7, 0.0))
+            resetMovieResolution()
+        else:
+            cmds.textField(ffmpegTextField, edit=True, backgroundColor=(0.7, 0.0, 0.0))
+            cmds.confirmDialog(
+                    title='Error: FFmpeg not found', 
+                    message='FFmpeg was not found, please specify the path to the ffmpeg executable',
+                    button='OK')
+
+    def browseFFMpeg(*args):
+        filename = cmds.fileDialog2(fileMode=1, fileFilter='*', caption="Select FFMpeg executable")
+        if filename is None:
+            return
+        path = os.path.abspath(filename[0])
+        cmds.textField(ffmpegTextField, edit=True, text=path)
+        settings = readSettings()
+        settings['ffmpegCommand'] = path
+        writeSettings(settings)
+        checkFFMpeg()
+
+    def getSelectedOperatingSystem():
+        index = cmds.radioButtonGrp(osRadioGroup, q=True, select=True)
+        if index == 1:
+            return 'PC'
+        else:
+            return 'MAC'
+
+    def onSelect(operatingSystem):
+        def handler(*args):
+            settings = readSettings()
+            settings['operatingSystem'] = operatingSystem
+            if 'ffmpegCommand' in settings:
+                del settings['ffmpegCommand']
+            writeSettings(settings)
+            cmds.textField(ffmpegTextField, edit=True, text=getDefaultFFMpeg(operatingSystem))
+            checkFFMpeg()
+        return handler
+
+    ffmpegFrame = cmds.frameLayout(label='FFMpeg', collapsable=True, parent=l)
+
+    osRadioGroup = cmds.radioButtonGrp(parent=ffmpegFrame, numberOfRadioButtons=2, label='    OS:           ', labelArray2=('PC', 'MAC'), 
+        select=1 if getDefaultOperatingSystem() == 'PC' else 2, 
+        onCommand1=onSelect('PC'), onCommand2=onSelect('MAC'), columnAlign=(1, 'left'), columnWidth3=(100, 80, 80))
+    row = cmds.rowLayout(parent=ffmpegFrame, numberOfColumns=2, columnWidth2=(290, 50), columnAttach2=('both', 'both'))
+    ffmpegTextField = cmds.textField(parent=row, editable=False, text=getDefaultFFMpeg(getSelectedOperatingSystem()))
+    browseFFMpegButton = cmds.button(label='Browse', parent=row, command=browseFFMpeg)
+
+    def browseInput(*args):
+        filename = cmds.fileDialog2(fileMode=1, caption="Select Movie File")
+        if filename is None:
+            return
+        path = os.path.abspath(filename[0])
+        cmds.textField(inputTextField, edit=True, text=path)
+        resetMovieResolution()
+
+    pathsFrame = cmds.frameLayout(label='Paths', parent=l)
+
+    currentSettings = readSettings()
+    row = cmds.rowLayout(parent=pathsFrame, numberOfColumns=3, columnWidth3=(90, 190, 50), columnAttach3=('both', 'both', 'both'))
+    cmds.text('Input Movie:', parent=row)
+    inputTextField = cmds.textField(parent=row, editable=False)
+    browseInputButton = cmds.button(label='Browse', parent=row, command=browseInput)
+    if 'inputMovie' in currentSettings:
+        cmds.textField(inputTextField, edit=True, text=currentSettings['inputMovie'])
+    
+    def browseOutput(*args):
+        filename = cmds.fileDialog2(fileMode=3, caption="Select Output Directory")
+        if filename is None:
+            return
+        path = os.path.abspath(filename[0])
+        cmds.textField(outputTextField, edit=True, text=path)
+
+    row = cmds.rowLayout(parent=pathsFrame, numberOfColumns=3, columnWidth3=(90, 190, 50), columnAttach3=('both', 'both', 'both'))
+    cmds.text('Output Directory:', parent=row)
+    outputTextField = cmds.textField(parent=row)
+    browseOutputButton = cmds.button(label='Browse', parent=row, command=browseOutput)
+    if 'outputDirectory' in currentSettings:
+        cmds.textField(outputTextField, edit=True, text=currentSettings['outputDirectory'])
+
+    imageOptionsFrame = cmds.frameLayout(label='Image Options', collapsable=True, parent=l)
+    sourceSizeText = cmds.text('Source Size: Unknown', parent=imageOptionsFrame)
+    row = cmds.rowLayout(parent=imageOptionsFrame, numberOfColumns=5, columnWidth5=(50, 50, 50, 50, 120), columnAttach5=('both', 'both', 'both', 'both', 'both'))
+    cmds.text('Width:', parent=row)
+    widthTextField = cmds.textField(parent=row)
+    cmds.text('Height: ', parent=row)
+    heightTextField = cmds.textField(parent=row)
+    cmds.checkBox(value=True, label='Keep Proportions', parent=row)
+
+    row = cmds.rowLayout(parent=imageOptionsFrame, numberOfColumns=3, columnWidth3=(60, 100, 160), columnAttach3=('both', 'both', 'both'))
+    cmds.text('File Name:', parent=row)
+    imageFileNameTextField = cmds.textField(parent=row, text='frame')
+    numDigitsMenu = cmds.optionMenu(label='  Frame Digits: ', parent=row)
+    cmds.menuItem(label='1')
+    cmds.menuItem(label='2')
+    cmds.menuItem(label='3')
+    cmds.menuItem(label='4')
+    cmds.optionMenu(numDigitsMenu, edit=True, select=4)
+
+    imageFileFormatMenu = cmds.optionMenu(label='File Format:', parent=imageOptionsFrame)
+    cmds.menuItem(label='PNG')
+    cmds.menuItem(label='JPEG')
+    if 'imageFileFormat' in currentSettings:
+        if currentSettings['imageFileFormat'] == 'JPEG':
+            cmds.optionMenu(imageFileFormatMenu, edit=True, select=2)
+        else:
+            cmds.optionMenu(imageFileFormatMenu, edit=True, select=1)
+
+    outputLogFrame = cmds.frameLayout(label='Output Log', collapsable=True, parent=l)
+    outputLog = cmds.scrollField(editable=False, wordWrap=True, parent=outputLogFrame)
+
+    def appendToLog(msg):
+        cmds.scrollField(outputLog, edit=True, insertText=msg, insertionPosition=0)
+
+    def resetUIEnabled():
+        cmds.button(convertButton, edit=True, label='Convert', command=convertMovie, parent=l)
+        cmds.radioButtonGrp(osRadioGroup, edit=True, enable=True)
+        cmds.button(browseFFMpegButton, edit=True, enable=True)
+        cmds.textField(inputTextField, edit=True, enable=True)
+        cmds.textField(outputTextField, edit=True, enable=True)
+        cmds.button(browseInputButton, edit=True, enable=True)
+        cmds.button(browseOutputButton, edit=True, enable=True)
+        cmds.optionMenu(imageFileFormatMenu, edit=True, enable=True)
+
+    def endWithSuccess():
+        resetUIEnabled()
+        cmds.confirmDialog(
+                title='Conversion successful', 
+                message='The input movie has been successfully converted into the image sequence.',
+                button='OK')
+
+    def endWithCancel():
+        resetUIEnabled()
+        
+    def endWithFailure():
+        resetUIEnabled()
+        cmds.confirmDialog(
+                title='Error: Conversion failed', 
+                message='The movie conversion failed. Please check the log for more details.',
+                button='OK')
+
+    def convertThread(ffmpegCommand, inputMoviePath, outputImagesDir, imageFormat, cancelEvent):
+        cmd = [ffmpegCommand, '-nostdin', '-y', '-i', inputMoviePath, os.path.join(outputImagesDir, 'frame%d.' + imageFormat)]
+        outputFilePath = getOutputLogPath()
+        with open(outputFilePath, 'w') as outputFd:
+            p = popen(cmd, stdout=outputFd, stderr=outputFd)
+            while True:
+                if cancelEvent.is_set():
+                    maya.utils.executeInMainThreadWithResult(endWithCancel)
+                    p.terminate()
+                    break
+                result = p.poll()
+                if result is not None:
+                    outputFd.close()
+                    with open(outputFilePath, 'r') as fd:
+                        logMsg = fd.read()
+                        appendToLog(logMsg)
+                        if result == 0:
+                            maya.utils.executeInMainThreadWithResult(endWithSuccess)
+                        else:
+                            maya.utils.executeInMainThreadWithResult(endWithFailure)
+                        break
+                time.sleep(0.1)
+
+    def convertMovie(*args):
+        global cancelSignal
+        ffmpegCommand = cmds.textField(ffmpegTextField, q=True, text=True)
+        if not isValidCommand(ffmpegCommand):
+            cmds.confirmDialog(
+                    title='Error: FFmpeg not found', 
+                    message='FFmpeg was not found, please specify the path to the ffmpeg executable',
+                    button='OK')
+            return
+        settings = readSettings()
+        inputMoviePath = cmds.textField(inputTextField, q=True, text=True)
+        outputImagesDir = cmds.textField(outputTextField, q=True, text=True)
+        imageFileFormat = cmds.optionMenu(imageFileFormatMenu, q=True, select=True)
+        settings['inputMovie'] = inputMoviePath
+        settings['outputDirectory'] = outputImagesDir
+        settings['imageFileFormat'] = 'PNG' if imageFileFormat == 1 else 'JPEG'
+        writeSettings(settings)
+        if imageFileFormat == 1:
+            imageFileFormat = 'png'
+        else:
+            imageFileFormat = 'jpg'
+        if len(inputMoviePath) == 0 or len(outputImagesDir) == 0:
+            cmds.confirmDialog(
+                title='Error: Missing input/output', 
+                message='Please specify both the input movie path and the output directory path.',
+                button='OK')
+            return
+        if not os.path.exists(inputMoviePath):
+            cmds.confirmDialog(
+                title='Error: Invalid input movie path', 
+                message='The given input movie path does not exist.',
+                button='OK')
+            return
+        if not os.path.exists(outputImagesDir):
+            cmds.confirmDialog(
+                title='Error: Invalid output directory path', 
+                message='The given output directory path does not exist.',
+                button='OK')
+            return
+        if not os.path.isdir(outputImagesDir):
+            cmds.confirmDialog(
+                title='Error: Invalid output directory path', 
+                message='The file at the given output directory path is not a directory.',
+                button='OK')
+            return
+        cmds.radioButtonGrp(osRadioGroup, edit=True, enable=False)
+        cmds.button(browseFFMpegButton, edit=True, enable=False)
+        cmds.textField(inputTextField, edit=True, enable=False)
+        cmds.textField(outputTextField, edit=True, enable=False)
+        cmds.button(browseInputButton, edit=True, enable=False)
+        cmds.button(browseOutputButton, edit=True, enable=False)
+        cmds.optionMenu(imageFileFormatMenu, edit=True, enable=False)
+        cmds.scrollField(outputLog, edit=True, text='')
+        def cancel(*args):
+            cancelEvent.set()
+        cmds.button(convertButton, edit=True, label='Cancel', command=cancel)
+        cancelEvent = threading.Event()
+        t = threading.Thread(target=convertThread, args=(ffmpegCommand, inputMoviePath, outputImagesDir, imageFileFormat, cancelEvent))
+        t.start()
+
+    convertButton = cmds.button(label='Convert', command=convertMovie, parent=l)
+    cmds.text(label='', parent=l)
+
+    cmds.showWindow(w)
+    checkFFMpeg()
